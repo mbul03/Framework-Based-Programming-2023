2b66340080b2f658244433db5ba6285e091e31f5
mbul03 <72655925+mbul03@users.noreply.github.com>
2023-04-04 13:24:56 +0700

    Add files via upload
    Commit Pengumpulan ETS

---
+++ Weather Application/Weather Application/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using static System.Net.WebRequestMethods;
+
+namespace Weather_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            getWeather();
+        }
+        string APIKey = "8193991d9f68c6d53d6f6a4734b8e443";
+        void getWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", TBCity.Text, APIKey);
+                var json = web.DownloadString(url);
+                WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+
+                picIcon.ImageLocation = "http://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+                labCondition.Text = Info.weather[0].main;
+                labDetails.Text = Info.weather[0].description;
+                labSunset.Text = convertDateTime(Info.sys.sunset).ToShortTimeString();
+                labSunrise.Text = convertDateTime(Info.sys.sunrise).ToShortTimeString();
+
+                labWindSpeed.Text = Info.wind.speed.ToString();
+                labPressure.Text = Info.main.pressure.ToString();
+            }
+        }
+        DateTime convertDateTime(long sec)
+        {
+            DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).ToLocalTime();
+            day = day.AddSeconds(sec).ToLocalTime();
+            return day;
+        }
+    }
+}
